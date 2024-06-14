f39d18af4c21b6da1305fe0158698356415b370d
MariaKhlestova <selebina.maria@yamdex.ru>
2024-06-14 15:18:43 +0300

    Lesson1-Task6

---
+++ Lesson1/Task6/Program.cs
@@ -0,0 +1,17 @@
+﻿int N = 1000;
+
+// создадим переменную счетчик i
+
+int i = 1;
+
+// цикл while - пока условие верно, выполяется ...
+
+while (i <= N)
+{
+    // Console.WriteLine(i);
+// Вывести в строку
+    Console.Write(i);
+    Console.Write(' ');
+    i = i +1;
+}
+
